7888e1b2481cbbb56a33649f6a0bf098c6b1b88d
karasiy <154829458+karasiy@users.noreply.github.com>
2026-04-20 14:03:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function saveTasks(tasks) {
 function createItem(item) {
   const template = document.getElementById("to-do__item-template");
   const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+  
   const textElement = clone.querySelector(".to-do__item-text");
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
